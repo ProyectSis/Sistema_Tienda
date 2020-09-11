0ab7ef5c82edd89f691336e0eebddae335248643
Luchoman2000 <luisangellibra2000@gmail.com>
2020-09-11 16:10:26 -0500

    test

--- src/sistema_tienda/Sistema_Tienda.java
+++ src/sistema_tienda/Sistema_Tienda.java
@@ -15,7 +15,8 @@ public class Sistema_Tienda {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola Mundo");
+        
     }
     
 }
