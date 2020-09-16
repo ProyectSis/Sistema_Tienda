6428731b689aff80e040e04228e4c4984c845036
Luchoman2000 <luisangellibra2000@gmail.com>
2020-09-16 13:38:34 -0500

    Algunas ventanas

--- src/sistema_tienda/Sistema_Tienda.java
+++ src/sistema_tienda/Sistema_Tienda.java
@@ -17,7 +17,9 @@ public class Sistema_Tienda {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hola xdxd");
         System.out.println("Hola Mundo");
+        
         conexion con = new conexion();         //Creamos un objeto de tipo conexion
         con.conectado();                    //Mandamos a llamar el metodo conectado
         
