db315d91ed07864db33804dfc301c432e07f7449
Luchoman2000 <luisangellibra2000@gmail.com>
2020-09-16 20:34:06 -0500

    acctualizacion de la base

--- src/modelos/conexion.java
+++ src/modelos/conexion.java
@@ -10,7 +10,9 @@ import java.sql.SQLException;
 
 public class conexion {
     private final String hostname ="tienda-db.cftnpf71qyzx.us-east-1.rds.amazonaws.com"; //Direccion de la base de datos
-    private final String url = "jdbc:mysql://"+hostname+":3306/tienda?user=root&password=12345678"; //Direccion de la  coneccion
+    //private final String url = "jdbc:mysql://"+hostname+":3306/tienda?user=root&password=12345678"; //Direccion de la  coneccion
+    private final String url ="jdbc:mysql://tienda.mysql.database.azure.com:3306/tienda?useSSL=true&requireSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"; 
+    //myDbConn = DriverManager.getConnection(url, "admin123@tienda", {Tango2000.});
     PreparedStatement prepareStatement;
     Connection conn = null;
     
@@ -18,7 +20,7 @@ public class conexion {
      try{   //Inicio del try
          
          Class.forName("com.mysql.cj.jdbc.Driver");     //Con el metodo de la clase forName, le pasamos el driver de MySQL para que lo cargue    
-         conn = DriverManager.getConnection(url);    //Apuntamos nuestro objeto con a el intento de conectarse con los parametros o las credenciales que tenemos en MYSQL
+         conn = DriverManager.getConnection(url, "admin123@tienda","Tango2000.");    //Apuntamos nuestro objeto con a el intento de conectarse con los parametros o las credenciales que tenemos en MYSQL
         //Aqui mandamos la url donde viene la direccion de la BD, nuestro nombre de usuario y la contraseña, que por defecto al instalar viene vacia
         if (conn!=null){                         //Si logramos conectarnos, con deja de apuntar a null y obtenemos conexion
             System.out.println("Conexión a base de datos funcionando");                //Sin funciona imprimimos en consola un mensaje
